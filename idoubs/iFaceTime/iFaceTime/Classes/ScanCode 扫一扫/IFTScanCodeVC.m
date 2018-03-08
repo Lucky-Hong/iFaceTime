1ae081c9f6593ba04da43f0cff486b5786a94157
董国庆 <yesdgq@yahoo.com>
2018-03-08 16:54:44 +0800

    save

--- idoubs/iFaceTime/iFaceTime/Classes/ScanCode 扫一扫/IFTScanCodeVC.m
+++ idoubs/iFaceTime/iFaceTime/Classes/ScanCode 扫一扫/IFTScanCodeVC.m
@@ -17,6 +17,11 @@
 //
 
 #import "IFTScanCodeVC.h"
+//#import "DTHScanQRCodesVC.h"
+//#import "LBXScanResult.h"
+//#import "LBXScanWrapper.h"
+#import "LBXScanVideoZoomView.h"
+#import "LBXScanViewController.h"
 
 @interface IFTScanCodeVC ()
 
@@ -38,12 +43,27 @@
 }
 
 - (void)setupUI {
+    self.view.backgroundColor = [UIColor blackColor];
+    // 返回键
     UIButton *goBackBtn = [UIButton buttonWithType:UIButtonTypeCustom];
     goBackBtn.frame = CGRectMake(10, 30, 30, 20);
     goBackBtn.enlargedEdge = 15;
     [goBackBtn setImage:[UIImage imageNamed:@"GoBack"] forState:UIControlStateNormal];
     [goBackBtn addTarget:self action:@selector(goBack:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:goBackBtn];
+    // titile
+    UILabel *titleLabel = [[UILabel alloc] init];
+    titleLabel.text = @"扫一扫";
+    titleLabel.textColor = [UIColor whiteColor];
+    titleLabel.font = [UIFont systemFontOfSize:17.f];
+    titleLabel.textAlignment = NSTextAlignmentCenter;
+    titleLabel.backgroundColor = [UIColor clearColor];
+    [self.view addSubview:titleLabel];
+    [titleLabel mas_updateConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.view);
+        make.centerY.equalTo(goBackBtn);
+        make.size.mas_equalTo(CGSizeMake(100, 25));
+    }];
 }
 
 - (void)goBack:(UIButton *)sender {
